bbee581b97a083cb5f0912afa70113d20d3af899
Vasco Orey <vasco.orey@gmail.com>
2013-07-13 11:45:20 +0100

    Initial Commit

---
+++ Cellular-Sounds/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  Cellular-Sounds
+//
+//  Created by Vasco Orey on 7/13/13.
+//  Copyright (c) 2013 Delta Dog Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cellular-SoundsTests/Cellular_SoundsTests.m
@@ -0,0 +1,36 @@
+//
+//  Cellular_SoundsTests.m
+//  Cellular-SoundsTests
+//
+//  Created by Vasco Orey on 7/13/13.
+//  Copyright (c) 2013 Delta Dog Studios. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Cellular_SoundsTests : XCTestCase
+
+@end
+
+@implementation Cellular_SoundsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
